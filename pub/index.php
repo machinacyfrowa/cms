dc0713831ce910461c946a2680204c8706c370d4
pzawadzki <admin@machinacyfrowa.pl>
2023-03-08 14:59:25 +0100

    initial twig implementation

--- pub/index.php
+++ pub/index.php
@@ -4,7 +4,15 @@ require_once('./../src/config.php');
 use Steampixel\Route;
 
 Route::add('/', function() {
-    echo "Strona główna";
+    //strona wyświetlająca obrazki
+    global $twig;
+    $twig->display("index.html.twig");
+});
+
+Route::add('/upload', function() {
+    //strona z formularzem do wgrywania obrazków
+    global $twig;
+    $twig->display("upload.html.twig");
 });
 
 Route::run('/cms/pub');
--- src/config.php
+++ src/config.php
@@ -2,5 +2,8 @@
 require_once('./../vendor/autoload.php');
 $db = new mysqli("localhost", "root", "", "cms");
 require("Post.class.php");
-
+//loader to taki pomocnik do ładowania szablonów
+$loader = new Twig\Loader\FilesystemLoader("./../src/templates");
+//inicjujemy twiga
+$twig = new Twig\Environment($loader);
 ?>
